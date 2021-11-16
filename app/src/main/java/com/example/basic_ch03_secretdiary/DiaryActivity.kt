a74914d883c5d73da601e8bee3526a19ffafe4e4
Yangsuhyuck <pplloo748@gmail.com>
2021-11-16 17:14:13 +0900

    2021 - 11 - 16 비밀 다이어리 복습

--- app/src/main/java/com/example/basic_ch03_secretdiary/DiaryActivity.kt
+++ app/src/main/java/com/example/basic_ch03_secretdiary/DiaryActivity.kt
@@ -22,8 +22,7 @@ class DiaryActivity : AppCompatActivity() {
 
         val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
 
-
-
+        // EditText에 적힌 Text를 저장하는 runnable
         val runnable = Runnable {
             getSharedPreferences("diary", Context.MODE_PRIVATE).edit() {
                 putString("detail",diaryBinding.diaryEditText.text.toString())
@@ -32,7 +31,11 @@ class DiaryActivity : AppCompatActivity() {
 
         diaryBinding.diaryEditText.setText(detailPreferences.getString("detail",""))
 
+        // EditText에서 Text가 변경될 때 마다 불러오는 Listener
         diaryBinding.diaryEditText.addTextChangedListener {
+            // 기존 runnable을 삭제 후 0.5초 뒤 텍스트를 저장할 runnable을 실행
+            // Text를 계속 입력할 경우 저장하지 않고 runnable을 삭제하면서 실시간 저장을 막아준다.
+            // Text를 다 입력하고 0.5초 동안 addTextChangedListener가 불리지 않을 경우 runnable 실행
             handler.removeCallbacks(runnable)
             handler.postDelayed(runnable, 500)  // 아무 입력이 없고 0.5초 후 자동으로 저장
         }
--- app/src/main/java/com/example/basic_ch03_secretdiary/MainActivity.kt
+++ app/src/main/java/com/example/basic_ch03_secretdiary/MainActivity.kt
@@ -16,12 +16,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
 
     private var changePasswordMode = false    // 비밀번호 변경 버튼 클릭 후 다른 처리를 막는 예외 처리용
+    // 비밀번호 변경 버튼을 한번 누르면 True , 새로운 비밀번호 설정 후 버튼을 누르면 False로 다시 변경
+    // 처음에 false 상태에서 저장된 비밀번호와 현재 입력한 비밀번호가 일치하는 지 확인하는 작업 실행 후 변경 작업 시작
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        // 각 자리수는 0~9까지만 허용
         mainBinding.numberPicker1.maxValue = 9
         mainBinding.numberPicker1.minValue = 0
 
@@ -31,17 +34,22 @@ class MainActivity : AppCompatActivity() {
         mainBinding.numberPicker3.maxValue = 9
         mainBinding.numberPicker3.minValue = 0
 
+        // 다이어리 열기 버튼 클릭 이벤트
         mainBinding.openBtn.setOnClickListener {
 
+            // 비밀번호 변경 중일 경우 , 변경 버튼 빨간 불
             if (changePasswordMode) {
                 Toast.makeText(this , "비밀번호를 변경 중입니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
+            // 저장된 비밀번호를 불러온다.
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
 
+            // User가 입력한 패스워드 변수로 저장
             val passwordFromUser = "${mainBinding.numberPicker1.value}${mainBinding.numberPicker2.value}${mainBinding.numberPicker3.value}"
 
+            // 저장됐던 비밀번호와 입력한 비밀번호가 같을 경우
             if (passwordPreferences.getString("password","000").equals(passwordFromUser))    // 키와 디폴트값 선언
             {
                 // 패스워드 성공
@@ -53,31 +61,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // 비밀번호 변경하는 버튼 클릭 이벤트
         mainBinding.changePasswordBtn.setOnClickListener {
 
+            // 저장됐던 비밀번호를 불러온다.
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+            // User가 입력한 비밀번호
             val passwordFromUser = "${mainBinding.numberPicker1.value}${mainBinding.numberPicker2.value}${mainBinding.numberPicker3.value}"
 
+            // 번호를 변경하는 상태일 경우, 변경 버튼 빨간 불에서 검은 불로 변경하는 과정 즉 새로운 비밀번호를 저장
             if (changePasswordMode) {
                 // 번호를 저장하는 기능
-
-
                 passwordPreferences.edit(true) {
                     putString("password",passwordFromUser)
                     // commit() -> sharedPreferences 적용하는 두가지 방법 중 하나, 하나는 위에 edit(true)
                 }
 
-                changePasswordMode = false
-                mainBinding.changePasswordBtn.setBackgroundColor(Color.BLACK)
+                changePasswordMode = false    // 비밀번호 변경 후 상태 변경
+                mainBinding.changePasswordBtn.setBackgroundColor(Color.BLACK)    // 버튼의 색도 다시 검은색으로 변경
 
 
             } else {
                 // changePasswordMode가 활성화(false에서 True로 변함) -> 비밀번호가 맞는지 확인
                 if (passwordPreferences.getString("password","000").equals(passwordFromUser))    // 키와 디폴트값 선언
                 {
-                    changePasswordMode = true
+                    changePasswordMode = true    // 비밀번호를 변경하는 상태로 변환
                     Toast.makeText(this, "번경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
-                    mainBinding.changePasswordBtn.setBackgroundColor(Color.RED)
+                    mainBinding.changePasswordBtn.setBackgroundColor(Color.RED)    // 버튼의 배경 빨간색으로 변경
 
                 } else {
                     // 패스워드 실패
